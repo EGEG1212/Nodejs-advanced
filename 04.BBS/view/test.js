210b946d9727099ce4951b25a447ebac12c372be
Eungyeong <dana1anne@naver.com>
2020-10-14 14:10:23 +0900

    BBS app start

--- 04.BBS/view/test.js
+++ 04.BBS/view/test.js
@@ -1,18 +1,9 @@
+const template = require('./tem')
+
 module.exports.test = function() {
     return`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <title>Bootstrap Example</title>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-      <script src="/jquery/jquery.min.js"></script>
-      <script src="/popper/popper.min.js"></script>
-      <script src="/bootstrap/js/bootstrap.min.js"></script>
-    <body>
-    
+    <div class="container" style="margin-top: 90px;">
+      ${template.header()}
     <div class="container">  
       <h1>My Icons <i class="fas fa-heart"></i></h1>
       <p>An icon along with some text: <i class="fas fa-thumbs-up"></i></p> 
@@ -28,8 +19,7 @@ module.exports.test = function() {
       <i class="fas fa-air-freshener"></i>
     </div>
     
-    </body>
-    </html>
+    ${template.footer()}
 
     `;
 }
